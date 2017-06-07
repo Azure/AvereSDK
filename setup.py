fdb3d76b8cc61b4c50735401bdb4639bba797731
Jason Woodward <woodwardj@averesystems.com>
2017-06-07 14:45:57 -0400

    framework: update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,28 @@ with open("vFXT/version.py") as f:
     exec(f.read(), version)
 
 setup(name='vFXT',
-  version=version['__version__'],
-  description='vFXT interaction library',
-  url='http://www.averesystems.com',
-  author='Jason Woodward',
-  author_email='woodwardj@averesystems.com',
-  license='Proprietary',
-  install_requires = ['boto', 'oauth2client', 'google-api-python-client', 'requests>=2.8.0'],
-  packages=['vFXT'],
-  test_suite="tests",
-  scripts=['vfxt.py'],
+    version=version['__version__'],
+    description='''The Avere virtual FXT (vFXT) Python library and vfxt.py command line utility''',
+    long_description='''The Avere virtual FXT (vFXT) Python library and vfxt.py command line utility
+provide the ability to create, extend, destroy, start, and stop vFXT clusters in
+all supported cloud environments.
+
+Licensed under the Apache License, Version 2.0.''',
+    url='http://www.averesystems.com',
+    author='Jason Woodward',
+    author_email='woodwardj@averesystems.com',
+    license='Apache License, Version 2.0',
+    install_requires = ['boto', 'oauth2client', 'google-api-python-client', 'requests>=2.8.0'],
+    packages=['vFXT'],
+    test_suite="tests",
+    scripts=['vfxt.py'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+    ],
 )

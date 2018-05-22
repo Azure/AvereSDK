b3df6a2b21ecbeca5a76a18f87df8b5c5cda6fa5
Jason Woodward <woodwardj@averesystems.com>
2018-05-22 09:07:40 -0400

    framework: bump version to 0.4.0.0

--- vFXT/version.py
+++ vFXT/version.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2015-2018 Avere Systems, Inc.  All Rights Reserved.
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
-__version__ = "0.3.9.1"
-__version_info__ = (0, 3, 9, 1)
+__version__ = "0.4.0.0"
+__version_info__ = (0, 4, 0, 0)

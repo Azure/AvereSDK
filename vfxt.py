4f78a1708271632b2437afbe4317a37670fe3960
Jason Woodward <woodwardj@averesystems.com>
2019-01-04 10:23:55 -0500

    vfxt.py: use ambiguous python in shebang since py2 or py3 are valid

--- vfxt.py
+++ vfxt.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 # Copyright (c) 2015-2019 Avere Systems, Inc.  All Rights Reserved.
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.

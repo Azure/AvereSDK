ecd7a33da7506c126cb7857c4841800aa25c7f00
Jason Woodward <jason.woodward@microsoft.com>
2020-07-07 17:14:14 -0400

    bump version

--- vFXT/version.py
+++ vFXT/version.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2015-2020 Avere Systems, Inc.  All Rights Reserved.
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
-__version__ = "0.5.4.2"
-__version_info__ = (0, 5, 4, 2)
+__version__ = "0.5.4.3"
+__version_info__ = (0, 5, 4, 3)

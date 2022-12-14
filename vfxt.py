efb3dcb83ec3f6654847966f6c60b9490a614859
Anne Latsko <anlatsko@microsoft.com>
2022-12-14 08:22:49 -0500

    Use azure-cli-core==2.43.0 and azure-mgmt-authorization==3.0.0
    - use DefaultAzureCredential instead of specifying specific credentials
    - set AZURE_APPLICATION_ID, AZURE_APPLICATION_SECRET, and AZURE_TENANT_ID
      to allow DefaultAzureCredential to create an EnvironmentCredential when
      --from-environment or --on-instance are not passed
    - use the 2022-04-01 authorization API version
    - misc cleanup to accommodate these version changes
    - fetch_subscription_for_resource_group() can now take a credential instead
      of always creating one; DefaultAzureCredential created by default

--- vfxt.py
+++ vfxt.py
@@ -8,6 +8,7 @@ import argparse
 import base64
 import logging
 import getpass
+import os
 import ssl
 import sys
 import uuid
@@ -362,6 +363,13 @@ def main():
                 if not all([args.subscription_id, args.azure_network, args.azure_subnet, args.resource_group, args.location]):
                     logger.error("Arguments subscription-id, azure-network, azure-subnet, resource-group, and location are required")
                     parser.exit(1)
+
+
+                # set these env vars based on the credentials passed into vfxt.py
+                # DefaultAzureCredential will use them to create an EnvironmentCredential
+                os.environ['AZURE_TENANT_ID'] = args.tenant_id
+                os.environ['AZURE_CLIENT_ID'] = args.application_id
+                os.environ['AZURE_CLIENT_SECRET'] = args.application_secret
 
             opts = {
                 'subscription_id': args.subscription_id,
